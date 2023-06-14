19a9f56d4a9c11b7b794ba7de76ba6bfe822fba8
Matias Hernandez <matias.hernandez.ca@usach.cl>
2023-06-13 23:40:49 -0400

    Funciones definidas para Monte Carlo

--- EP11-G2.R
+++ EP11-G2.R
@@ -3,9 +3,132 @@
 # Integrante Matías Hernández
 # Integrante Ariel Núñez
 # Integrante Carla Polanco
-
+library(ggpubr)
 
 datos <- read.csv2(file.choose(), stringsAsFactors = TRUE)
+
+# --------------Definicion de funciones importantes--------------
+
+# Función para obtener una permutación.
+# Argumentos:
+# - i: iterador (para llamadas posteriores).
+# - muestra_1, muestra_2: muestras.
+# Valor:
+# - lista con las muestras resultantes tras la permutación.
+obtiene_permutacion <- function(i, muestra_1, muestra_2) {
+  n_1 <- length(muestra_1)
+  combinada <- c(muestra_1, muestra_2)
+  n <- length(combinada)
+  permutacion <- sample(combinada, n, replace = FALSE)
+  nueva_1 <- permutacion[1:n_1]
+  nueva_2 <- permutacion[(n_1+1):n]
+  return(list(nueva_1, nueva_2))
+}
+
+# Función para calcular la diferencia de un estadístico de interés entre las
+# dos muestras.
+# Argumentos:
+# - muestras: lista con las muestras.
+# - FUN: nombre de la función que calcula el estadístico de interés.
+# Valor:
+# - diferencia de un estadístico para dos muestras.
+calcular_diferencia <- function(muestras, FUN) {
+  muestra_1 <- muestras[[1]]
+  muestra_2 <- muestras[[2]]
+  diferencia <- FUN(muestra_1) - FUN(muestra_2)
+  return(diferencia)
+}
+
+# Función para calcular el valor p.
+# Argumentos:
+# - distribucion: distribución nula del estadístico de interés.
+# - valor_observado: valor del estadístico de interés para las muestras
+#   originales.
+# - repeticiones: cantidad de permutaciones a realizar.
+# - alternative: tipo de hipótesis alternativa. "two.sided" para
+#   hipótesis bilateral, "greater" o "less" para hipótesis unilaterales.
+# Valor:
+# - el valorp calculado.
+calcular_valor_p <- function(distribucion, valor_observado,
+                             repeticiones, alternative) {
+  if(alternative == "two.sided") {
+    numerador <- sum(abs(distribucion) > abs(valor_observado)) + 1
+    denominador <- repeticiones + 1
+    valor_p <- numerador / denominador
+  }
+  else if(alternative == "greater") {
+    numerador <- sum(distribucion > valor_observado) + 1
+    denominador <- repeticiones + 1
+    valor_p <- numerador / denominador
+  }
+  else {
+    numerador <- sum(distribucion < valor_observado) + 1
+    denominador <- repeticiones + 1
+    valor_p <- numerador / denominador
+  }
+  
+  return(valor_p)
+}
+
+# Función para graficar una distribución.
+# Argumentos:
+# - distribucion: distribución nula del estadístico de interés.
+# - ...: otros argumentos a ser entregados a gghistogram y ggqqplot.
+graficar_distribucion <- function(distribucion, ...) {
+  observaciones <- data.frame(distribucion)
+  
+  histograma <- gghistogram(observaciones, x = "distribucion",
+                            xlab = "Estadístico de interés",
+                            ylab = "Frecuencia", bins = 30, ...)
+  
+  qq <- ggqqplot(observaciones, x = "distribucion", ...)
+  
+  # Crear una única figura con todos los gráficos de dispersión.
+  figura  <- ggarrange(histograma, qq, ncol = 2, nrow = 1)
+  print(figura)
+}
+
+# Función para hacer la prueba de permutaciones.
+# Argumentos:
+# - muestra_1, muestra_2: vectores numéricos con las muestras a comparar.
+# - repeticiones: cantidad de permutaciones a realizar.
+# - FUN: función del estadístico E para el que se calcula la diferencia.
+# - alternative: tipo de hipótesis alternativa. "two.sided" para
+#   hipótesis bilateral, "greater" o "less" para hipótesis unilaterales.
+# - plot: si es TRUE, construye el gráfico de la distribución generada.
+# - ...: otros argumentos a ser entregados a graficar_distribucion.
+contrastar_hipotesis_permutaciones <- function(muestra_1, muestra_2,
+                                               repeticiones, FUN,
+                                               alternative, plot, ...) {
+  cat("Prueba de permutaciones\n\n")
+  cat("Hipótesis alternativa:", alternative, "\n")
+  observado <- calcular_diferencia(list(muestra_1, muestra_2), FUN)
+  cat("Valor observado:", observado, "\n")
+  
+  n_1 <- length(muestra_1)
+  
+  # Generar permutaciones.
+  permutaciones <- lapply(1:repeticiones, obtiene_permutacion, muestra_1,
+                          muestra_2)
+  
+  # Generar la distribución.
+  distribucion <- sapply(permutaciones, calcular_diferencia, FUN)
+  
+  # Graficar la distribución.
+  if(plot) {
+    graficar_distribucion(distribucion, ...)
+  }
+  
+  # Calcular el valor p.
+  valor_p <- calcular_valor_p(distribucion, observado, repeticiones,
+                              alternative)
+  
+  cat("Valor p:", valor_p, "\n\n")
+}
+
+
+
+
 
 
 # PREGUNTAS (TODOS LOS GRUPOS) 
@@ -14,9 +137,17 @@ datos <- read.csv2(file.choose(), stringsAsFactors = TRUE)
 # muestra aleatoria de hogares (250 < n < 500) y respondan la pregunta propuesta utilizando una simulación 
 # Monte Carlo. 
 
+# Pregunta de investigación original propuesta:
+# Un grupo de estudiantes de informatica de la universidad de Santiago de Chile quiere saber si 
+# 
 
-
-
+# Tamaño muestra = 400
+# Semilla = 1312
+# alfa = 0.05
+# Permutaciones = 1750
+R = 1750
+set.seed(1312)
+# media_valor_observado <- mean(datos)
 
 
 
